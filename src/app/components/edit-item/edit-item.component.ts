3e11b54466cee6f687360e597606286a66b0cfa6
DenKorablev <denis18051990@mail.ru>
2019-01-24 20:52:09 +0300

    Начал тестовое задание

--- src/app/components/edit-item/edit-item.component.ts
+++ src/app/components/edit-item/edit-item.component.ts
@@ -14,12 +14,14 @@ export class EditItemComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.email])
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required]),
+      'name': new FormControl(null, [Validators.required])
     });
 
   }
 
   onSubmit() {
-
+    console.log(this.form);
   }
 }

2268f5b89336dfd02383497dbec71ef1468314aa
DenKorablev <denis18051990@mail.ru>
2019-01-27 15:06:40 +0300

    Убрал не корректные данные

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -1,7 +1,7 @@
 export class Item {
     constructor(
-        public name: string = "12",
-        public price: number = 0,
+        public name: string,
+        public price: number,
         public count: number,
         public photo: string,
         public id?: number

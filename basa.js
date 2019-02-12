17b2ff2b72b820d473cafa7aa111b0c3273db07e
DenKorablev <denis18051990@mail.ru>
2019-02-12 10:17:25 +0300

    Изображение в теге Image, добавил красивую кнопку загрузки изображений

--- basa.js
+++ basa.js
@@ -6,49 +6,49 @@ module.exports = function () {
                 name: "Лыжи", 
                 price: 1000,                  
                 count: 3, 
-                photo: "assets\\images\\item1.jpg"
+                photo: "assets//images//item1.jpg"
             },             
             { 
                 id: 2, 
                 name: "Коньки", 
                 price: 1050,                  
                 count: 5, 
-                photo: "assets\\images\\item2.jpg"
+                photo: "assets//images//item2.jpg"
             }, 
             { 
                 id: 3, 
                 name: "Хоккейные клюшки", 
                 price: 500,                  
                 count: 47, 
-                photo: "assets\\images\\item3.jpg"
+                photo: "assets//images//item3.jpg"
             }, 
             { 
                 id: 4, 
                 name: "Шапки", 
                 price: 150,                  
                 count: 78, 
-                photo: "assets\\images\\item4.jpg"
+                photo: "assets//images//item4.jpg"
             }, 
             { 
                 id: 5, 
                 name: "Мячи", 
                 price: 300,                  
                 count: 3, 
-                photo: "assets\\images\\item5.jpg"
+                photo: "assets//images//item5.jpg"
             }, 
             { 
                 id: 6, 
                 name: "Вратарская форма", 
                 price: 700,                  
                 count: 5, 
-                photo: "assets\\images\\item6.jpg"
+                photo: "assets//images//item6.jpg"
             }, 
             { 
                 id: 7, 
                 name: "Гантели", 
                 price: 250,                  
                 count: 15, 
-                photo: "assets\\images\\item7.jpg"
+                photo: "assets//images//item7.jpg"
             }                   
         ] 
     } 

ef41bf918b480e67ffe4347999f66b6c329fd4ef
DenKorablev <denis18051990@mail.ru>
2019-03-13 13:36:27 +0300

    Реализовал темы в библиотеке компонентов

---
+++ library-base/projects/theme/src/lib/theme.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'theme-theme',
+  template: `
+    <p>
+      theme works!
+    </p>
+  `,
+  styles: []
+})
+export class ThemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ library-base/projects/theme/src/lib/theme.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ThemeComponent } from './theme.component';
+
+@NgModule({
+  declarations: [ThemeComponent],
+  imports: [
+  ],
+  exports: [ThemeComponent]
+})
+export class ThemeModule { }
